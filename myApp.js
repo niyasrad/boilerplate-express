3fdda29e8711a3aa815a6812d61a0abd658223ab
Niyas Hameed <84234554+niyasrad@users.noreply.github.com>
2023-02-10 16:40:33 +0530

    [Niyas] JSON Request Manipulation- Middleware

--- myApp.js
+++ myApp.js
@@ -1,5 +1,20 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
+// app.get('/', (req, res) => res.send("Hello Express"))
+let absolutePath = __dirname + '/views/index.html';
+app.use((req, res, next) => {
+    console.log(req.method + " " + req.path + " - " + req.ip)
+    next();
+})
+app.get('/', (req, res) => res.sendFile(absolutePath));
+
+let message = "Hello json";
+
+app.get('/json', (req, res) => res.json({"message" : (process.env.MESSAGE_STYLE === "uppercase" ? message.toUpperCase() : message)}))
+app.use('/public', express.static(__dirname + '/public'))
+
+console.log("Hello World")
 
 
 
@@ -34,6 +49,4 @@ let app = express();
 
 
 
-
-
- module.exports = app;
+module.exports = app;
