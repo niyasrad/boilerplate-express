264ed1e2048f80a31d8c76a3320c24910ce67669
Niyas Hameed <84234554+niyasrad@users.noreply.github.com>
2023-02-11 19:36:26 +0530

    [Niyas] Completed Node.js

--- myApp.js
+++ myApp.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
+let bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
 // app.get('/', (req, res) => res.send("Hello Express"))
 let absolutePath = __dirname + '/views/index.html';
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 app.use((req, res, next) => {
     console.log(req.method + " " + req.path + " - " + req.ip)
     next();
@@ -14,7 +17,19 @@ let message = "Hello json";
 app.get('/json', (req, res) => res.json({"message" : (process.env.MESSAGE_STYLE === "uppercase" ? message.toUpperCase() : message)}))
 app.use('/public', express.static(__dirname + '/public'))
 
-console.log("Hello World")
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({"time" : req.time});
+})
+
+app.get('/:word/echo', (req, res) => res.json({"echo": req.params.word}))
+
+app.get('/name', (req, res) => res.json({"name": req.query.first + " " +req.query.last}))
+app.post('/name', (req, res) => {
+    res.json({"name": req.body.first + " " +req.body.last}) 
+})
 
 
 
